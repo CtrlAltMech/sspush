f67b9abefa7c9e49743ee1a53c4f614834a0274e
mech <mech@dmux.xyz>
2023-06-10 19:36:46 -0400

    Works on linux. Super sloppy. Need to add the section for macOS. Clean up all commented stuff later

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ List of things to do:
 - Add argument/options to select a photo
 - Add general error handling
 - Check and generate a configuration file.
+- Add a function to check if the screenshots folder exists
 
--- sspush.sh
+++ sspush.sh
@@ -8,6 +8,17 @@
 # by mech
 #
 
+#Find the most recent file
+#TODO set up arguments to push stuff outside of most recent screenshot
+find_file() {
+	ls -Art $SSFILEPATH | tail -n 1
+}
+
+#Update the name to 10 random characters
+random_name () {
+	echo $RANDOM | md5sum | head -c 8; echo;
+}
+
 # Source the configuration file
 . $HOME/.sspushrc
 
@@ -16,8 +27,15 @@ if [ "$(uname -s)" == "Darwin" ]; then
 	echo "This is macOS"
 elif [ "$(uname -s)" == "Linux" ]; then
 	echo "This is Linux"
+	echo "Your most recent file is...$(find_file)"
+
+	filename=$(basename "$SSFILEPATH/$(find_file)") # Finds the most recent file and stores it in the filename variable.
+	mv "${SSFILEPATH}$(find_file)" "${SSFILEPATH}$(random_name).${filename##*.}" # Renames the file to random 8 character string
+	scp -i $KEYPATH ${SSFILEPATH}$(find_file) $USERNAME@$SERVER:$REMOTEDIR # Transfer that file to the remote server
+	echo "Link to file https://thinkingdirt.net/pics/$(find_file)" # Feedback to see the link
 else
 	echo "Well...what is this then?"
+fi
 
 #Starts interactive screencap and saves file
 #TODO Unable to get keybind to work properly in macOS, have not tried Linux yet.
@@ -26,31 +44,31 @@ else
 
 #Find the most recent file
 #TODO set up arguments to push stuff outside of most recent screenshot
-find_file() {
-	ls -Art $SSFILEPATH | tail -n 1
-}
+# find_file() {
+# 	ls -Art $SSFILEPATH | tail -n 1
+# }
 
-echo "Your most recent file is...$(find_file)"
+# echo "Your most recent file is...$(find_file)"
 
-#Update the name to 10 random characters
-random_name () {
-	echo $RANDOM | md5sum | head -c 8; echo;
-}
+# #Update the name to 8 random characters
+# random_name () {
+# 	echo $RANDOM | md5sum | head -c 8; echo;
+# }
 
 
-filename=$(basename "$SSFILEPATH/$(find_file)")
+# filename=$(basename "$SSFILEPATH/$(find_file)")
 
-mv "${SSFILEPATH}$(find_file)" "${SSFILEPATH}$(random_name).${filename##*.}"
+# mv "${SSFILEPATH}$(find_file)" "${SSFILEPATH}$(random_name).${filename##*.}"
 
-# Transfer the most recent file to the server
-scp -i $KEYPATH ${SSFILEPATH}$(find_file) $USERNAME@$SERVER:$REMOTEDIR
+# # Transfer the most recent file to the server
+# scp -i $KEYPATH ${SSFILEPATH}$(find_file) $USERNAME@$SERVER:$REMOTEDIR
 
-# Copy to macOS clipboard
-echo "$BASELINK$(find_file)" | pbcopy
+# # Copy to macOS clipboard
+# echo "$BASELINK$(find_file)" | pbcopy
 
-# Shows a notification that the file has been uploaded
-#TODO Set this as an option in the config
-#osascript -e 'display notification "File '$(find_file)' Uploaded!"'
+# # Shows a notification that the file has been uploaded
+# #TODO Set this as an option in the config
+# #osascript -e 'display notification "File '$(find_file)' Uploaded!"'
 
-# If ran in terminal this will show the link in stdout
-echo "Link to file https://thinkingdirt.net/pics/$(find_file)"
+# # If ran in terminal this will show the link in stdout
+# echo "Link to file https://thinkingdirt.net/pics/$(find_file)"
