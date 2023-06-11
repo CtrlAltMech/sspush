d5cd94ece9b036835b938489160a7f139fbdc0d2
CtrlAltMech <mech@dmux.xyz>
2023-06-11 13:06:15 -0400

    Added an argument, moved stuff under the appropriate conditional. Probably not gonna stay formatted like that, but it all works

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # sspush
 A simple script to push screenshots to a webserver for public viewing with a link
-
+--
 List of things to do:
-- Make it so the randomly generated name uses /dev/urandom so it will work on both mac and linux
-- Make it so the script will detect the OS and run on the proper if branch
 - Add argument/options to select a photo
 - Add general error handling
 - Check and generate a configuration file.
--- sspush.sh
+++ sspush.sh
@@ -8,13 +8,17 @@
 # by mech
 #
 
-#Find the most recent file
-#TODO set up arguments to push stuff outside of most recent screenshot
+if [[ -n $1 && "$1" == "-n" ]]; then
+	args="n"
+fi
+
+# Find the most recent file
+# TODO set up arguments to push stuff outside of most recent screenshot
 find_file() {
 	ls -Art $SSFILEPATH | tail -n 1
 }
 
-#Update the name to 10 random characters
+# Genterate 8 random characters
 random_name () {
 	echo $RANDOM | md5sum | head -c 8; echo;
 }
@@ -23,8 +27,26 @@ random_name () {
 . $HOME/.sspushrc
 
 # Determines what OS this is running on and will do something with that information
-if [ "$(uname -s)" == "Darwin" ]; then
+if [[ "$(uname -s)" == "Darwin" ]]; then
+	#Starts interactive screencap and saves file
+	#TODO Unable to get keybind to work properly in macOS, have not tried Linux yet.
+	# For now this will be triggered the usual way and sspush will need to be ran manually
+	#screencapture -i ${SSFILEPATH}none.jpg
 	echo "This is macOS"
+	echo "Your most recent file is...$(find_file)"
+
+	filename=$(basename "$SSFILEPATH/$(find_file)") # Finds the most recent file and stores it in the filename variable.
+	mv "${SSFILEPATH}$(find_file)" "${SSFILEPATH}$(random_name).${filename##*.}" # Renames the file to random 8 character string
+	scp -i $KEYPATH ${SSFILEPATH}$(find_file) $USERNAME@$SERVER:$REMOTEDIR # Transfer that file to the remote server
+	echo "$BASELINK$(find_file)" | pbcopy # Copies to macOS keyboard
+	echo "Link to file $BASELINK$(find_file)" # Feedback to see the link
+	
+	# Shows a notification that the file has been uploaded
+	#TODO Set this as an option in the config
+	if [[ "$args" == "n" ]]; then
+		osascript -e 'display notification "File '$(find_file)' Uploaded!"'
+	fi
+
 elif [ "$(uname -s)" == "Linux" ]; then
 	echo "This is Linux"
 	echo "Your most recent file is...$(find_file)"
@@ -32,43 +54,8 @@ elif [ "$(uname -s)" == "Linux" ]; then
 	filename=$(basename "$SSFILEPATH/$(find_file)") # Finds the most recent file and stores it in the filename variable.
 	mv "${SSFILEPATH}$(find_file)" "${SSFILEPATH}$(random_name).${filename##*.}" # Renames the file to random 8 character string
 	scp -i $KEYPATH ${SSFILEPATH}$(find_file) $USERNAME@$SERVER:$REMOTEDIR # Transfer that file to the remote server
-	echo "Link to file https://thinkingdirt.net/pics/$(find_file)" # Feedback to see the link
+	echo "$BASELINK$(find_file)" | xclip -i
+	echo "Link to file $BASELINK$(find_file)" # Feedback to see the link
 else
 	echo "Well...what is this then?"
 fi
-
-#Starts interactive screencap and saves file
-#TODO Unable to get keybind to work properly in macOS, have not tried Linux yet.
-# For now this will be triggered the usual way and sspush will need to be ran manually
-#screencapture -i ${SSFILEPATH}none.jpg
-
-#Find the most recent file
-#TODO set up arguments to push stuff outside of most recent screenshot
-# find_file() {
-# 	ls -Art $SSFILEPATH | tail -n 1
-# }
-
-# echo "Your most recent file is...$(find_file)"
-
-# #Update the name to 8 random characters
-# random_name () {
-# 	echo $RANDOM | md5sum | head -c 8; echo;
-# }
-
-
-# filename=$(basename "$SSFILEPATH/$(find_file)")
-
-# mv "${SSFILEPATH}$(find_file)" "${SSFILEPATH}$(random_name).${filename##*.}"
-
-# # Transfer the most recent file to the server
-# scp -i $KEYPATH ${SSFILEPATH}$(find_file) $USERNAME@$SERVER:$REMOTEDIR
-
-# # Copy to macOS clipboard
-# echo "$BASELINK$(find_file)" | pbcopy
-
-# # Shows a notification that the file has been uploaded
-# #TODO Set this as an option in the config
-# #osascript -e 'display notification "File '$(find_file)' Uploaded!"'
-
-# # If ran in terminal this will show the link in stdout
-# echo "Link to file https://thinkingdirt.net/pics/$(find_file)"
